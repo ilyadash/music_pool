d6e8ec77c5430d0c29631f37a9aae15565e23e6f
Ilya Dashkov <69359690+ilyadash@users.noreply.github.com>
2025-07-16 11:21:26 +0300

    Formatting by Ruff

--- src/main.py
+++ src/main.py
@@ -4,68 +4,88 @@ from music_poll_bot import MusicPollBot
 import sys
 
 CURRENT_DIRECTORY: str = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(CURRENT_DIRECTORY, 'utils'))
+sys.path.append(os.path.join(CURRENT_DIRECTORY, "utils"))
 
 import environment as env
 
 bot = MusicPollBot(env.get_bot_token())
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 async def send_welcome(message):
     bot.music_directory = env.get_music_directory()
     bot.set_volume(50)
     bot.volume_increment = env.get_volume_increment()
-    await bot.convert_all_to_mp3(env.get_music_directory(), env.get_music_playlist(), message)
+    await bot.convert_all_to_mp3(
+        env.get_music_directory(), env.get_music_playlist(), message
+    )
     await bot.set_playlist(env.get_music_playlist())
-    await bot.reply_to(message, "Welcome! I can play music from your local directory. Use /play to start or /help for help.")
+    await bot.reply_to(
+        message,
+        "Welcome! I can play music from your local directory. Use /play to start or /help for help.",
+    )
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=["help"])
 async def send_help(message):
     """Send a message with available commands."""
-    await bot.reply_to(message, "/play - Start playing music\n/next - Go to the next track\n/up - Increase volume\n/down - Decrease volume")
+    await bot.reply_to(
+        message,
+        "/play - Start playing music\n/next - Go to the next track\n/up - Increase volume\n/down - Decrease volume",
+    )
 
-@bot.message_handler(commands=['play'])
+
+@bot.message_handler(commands=["play"])
 async def play_music(message):
     """Play music from the local directory."""
     bot.shuffle_playlist()
     await bot.play_all(message)
 
-@bot.message_handler(commands=['pause'])
+
+@bot.message_handler(commands=["pause"])
 async def pause_music(message):
     await bot.pause(message)
-    
-@bot.message_handler(commands=['unpause'])
-async def unpause_music(message):
-    await bot.unpause(message)    
 
-@bot.message_handler(commands=['next'])
+
+@bot.message_handler(commands=["unpause"])
+async def unpause_music(message):
+    await bot.unpause(message)
+
+
+@bot.message_handler(commands=["next"])
 async def play_next_track(message):
     """Play the next track in the directory."""
-    await bot.play_next(message)        
+    await bot.play_next(message)
 
-@bot.message_handler(commands=['up'])
+
+@bot.message_handler(commands=["up"])
 async def increase_volume(message):
     """Increase the volume."""
     bot.up()
     await bot.reply_to(message, f"Volume increased to: {bot.current_volume}%")
 
-@bot.message_handler(commands=['down'])
+
+@bot.message_handler(commands=["down"])
 async def decrease_volume(message):
     """Decrease the volume."""
     bot.down()
     await bot.reply_to(message, f"Volume decreased to: {bot.current_volume}%")
 
-@bot.message_handler(commands=['stop'])
+
+@bot.message_handler(commands=["stop"])
 async def stop_play(message):
     await bot.stop(message)
-    
-@bot.message_handler(commands=['sound'])
-async def sound(message):
-    await bot.sound(message)
+
+
+@bot.message_handler(commands=["skip"])
+async def skip(message):
+    await bot.skip_track(message)
+
 
 # Run the bot using a loop (or use an event-driven method if you're in a non-blocking environment)
 async def main():
     await asyncio.gather(bot.infinity_polling(), bot.continue_playing())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- src/music_poll_bot.py
+++ src/music_poll_bot.py
@@ -8,197 +8,236 @@ import time
 import sys
 
 CURRENT_DIRECTORY: str = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(CURRENT_DIRECTORY, 'utils'))
+sys.path.append(os.path.join(CURRENT_DIRECTORY, "utils"))
 
 from convert import convert_to_mp3
 
-class MusicPollBot (AsyncTeleBot): # add code for wrapper class - to hold my additional data and methods
+
+class MusicPollBot(
+    AsyncTeleBot
+):  # add code for wrapper class - to hold my additional data and methods
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #TODO: Add poll methods
-        #TODO: Add admin only methods
+        # TODO: Add poll methods
+        # TODO: Add admin only methods
         pg.init()
         pg.mixer.init()
-        self.has_started: bool = False # flag that bot is "turned on"
-        self.state: str = '' # in what regime is bot right now
+        self.has_started: bool = False  # flag that bot is "turned on"
+        self.state: str = ""  # in what regime is bot right now
         self.playing: bool = False
-        self.music_directory = ''
-        self.ok_to_convert_extensions: list[str] = ['.m4a']
-        self.ok_to_play_extensions: list[str] = ['.mp3', '.wav', '.ogg']
+        self.music_directory = ""
+        self.ok_to_convert_extensions: list[str] = [".m4a"]
+        self.ok_to_play_extensions: list[str] = [".mp3", ".wav", ".ogg"]
         self.playlist: list[str] = []
-        self.current_file: str = ''
+        self.current_file: str = ""
         self.current_track_number: int = -1
-        self.current_volume = pg.mixer.music.get_volume() * 100 # in %
+        self.current_volume = pg.mixer.music.get_volume() * 100  # in %
         self.shuffled_playlist: bool = False
         self.track_tags = None
         self.volume_increment = 10
         self.start_playing: bool = False
         self.statistics = {
-            "tracks": {
-                "played": 0,
-                "skipped": 0
-            },
-            "polls": {
-                "started": 0,
-                "passed": 0
-            }
+            "tracks": {"played": 0, "skipped": 0},
+            "polls": {"started": 0, "passed": 0},
         }
+
     def check_file_exists(self, full_path) -> bool:
         return os.path.exists(full_path) and os.path.isfile(full_path)
+
     async def convert_to_mp3(self, dir, file, message_reply_to=None) -> bool:
         await convert_to_mp3(dir, file)
-        if message_reply_to != None:
+        if message_reply_to is not None:
             await self.reply_to(message_reply_to, f"Converted {file} to mp3")
-    async def convert_all_to_mp3(self, dir:str = '', files:list[str] = [], message_reply_to=None) -> None:
-        #TODO: Fix conversion to mp3 to work faster. In parallel, maybe?
-        files_to_convert:list[str] = []
-        if message_reply_to != None:
-            await self.reply_to(message_reply_to, f"Converting all files with unacceptable extensions to mp3")
-            
-        if dir == '':
+
+    async def convert_all_to_mp3(
+        self, dir: str = "", files: list[str] = [], message_reply_to=None
+    ) -> None:
+        # TODO: Fix conversion to mp3 to work faster. In parallel, maybe?
+        files_to_convert: list[str] = []
+        if message_reply_to is not None:
+            await self.reply_to(
+                message_reply_to,
+                "Converting all files with unacceptable extensions to mp3",
+            )
+
+        if dir == "":
             dir = self.music_directory
-            
+
         if len(files) == 0:
             files = self.playlist
-            
+
         for file in files:
             name, extension = os.path.splitext(file)
             if extension in self.ok_to_convert_extensions:
-                if not self.check_file_exists(dir + '\\' + name + '.mp3'):
+                if not self.check_file_exists(dir + "\\" + name + ".mp3"):
                     files_to_convert.append(file)
-                     
-        if message_reply_to != None:
+
+        if message_reply_to is not None:
             if len(files_to_convert) == 0:
-                await self.reply_to(message_reply_to, f"All files are already in mp3!")
+                await self.reply_to(message_reply_to, "All files are already in mp3!")
             else:
-                await self.reply_to(message_reply_to, f"Wait before conversion is ended!") 
-                    
+                await self.reply_to(
+                    message_reply_to, "Wait before conversion is ended!"
+                )
+
         if len(files_to_convert) > 0:
             conversion_tasks = [
                 self.convert_to_mp3(dir, file_name, message_reply_to)
                 for file_name in files_to_convert
             ]
-            
+
             await asyncio.gather(*conversion_tasks, return_exceptions=False)
-            
-            if message_reply_to != None:
-                await self.reply_to(message_reply_to, f"Finished converting {len(files_to_convert)} files to mp3")
+
+            if message_reply_to is not None:
+                await self.reply_to(
+                    message_reply_to,
+                    f"Finished converting {len(files_to_convert)} files to mp3",
+                )
+
     def shuffle_playlist(self) -> None:
         self.shuffled_playlist = True
         rnd.shuffle(self.playlist)
+
     def file_is_ok(self, file) -> bool:
         is_ok: bool = False
         if str(type(file)) == "<class 'str'>":
             name, extension = os.path.splitext(file)
-            if extension in self.ok_to_play_extensions: 
+            if extension in self.ok_to_play_extensions:
                 is_ok = True
         return is_ok
-    async def set_playlist(self, list_of_files:list[str]=[]) -> None:
+
+    async def set_playlist(self, list_of_files: list[str] = []) -> None:
         if len(list_of_files) == 0:
             return
         for file in list_of_files:
             if not self.file_is_ok(file):
                 list_of_files.remove(file)
         self.playlist = list_of_files
+
     async def play_all(self, message_reply_to=None) -> None:
-        if message_reply_to != None:
-            await self.reply_to(message_reply_to, f"Files in queue: {len(self.playlist)}")
+        if message_reply_to is not None:
+            await self.reply_to(
+                message_reply_to, f"Files in queue: {len(self.playlist)}"
+            )
             self.current_track_number = 0
-            if self.set_current_file(self.playlist[self.current_track_number]):
-                await self.play(self.current_file, message_reply_to=message_reply_to)
-            else:
-                await self.play_next(message_reply_to)
-    async def continue_playing(self, message_reply_to=None) -> bool:
+            await self.play()
+
+    async def continue_playing(self, message_reply_to=None) -> None:
         if self.start_playing:
             while True:
                 if not pg.mixer.music.get_busy():
                     await self.play(message_reply_to=message_reply_to)
-                    return True
-        return False
-    def load_file(self, file:str='', message_reply_to=None) -> bool:
+
+    def load_file(self, file: str = "", message_reply_to=None) -> bool:
         loaded_file = True
-        if file == '':
+        if file == "":
             file = self.current_file
         try:
             pg.mixer.music.load(os.path.join(self.music_directory, file))
         except pg.error:
-            if message_reply_to != None:
-                self.reply_to(message_reply_to, f"Could not load the music file. Ensure the file {self.current_file} exists and is a valid format.")
+            if message_reply_to is not None:
+                self.reply_to(
+                    message_reply_to,
+                    f"Could not load the music file. Ensure the file {self.current_file} exists and is a valid format.",
+                )
             loaded_file = False
         return loaded_file
-    async def play(self, file:str='', message_reply_to=None) -> bool:
+
+    async def play(self, file: str = "", message_reply_to=None) -> bool:
         self.playing = False
-        if file == '':
-            file = self.current_file
-        if self.set_current_file(file):   
+        if file == "":
+            if self.file_is_ok(self.current_file):
+                file = self.current_file
+            else:
+                file = self.playlist[self.current_track_number]
+        if self.set_current_file(file):
             if self.load_file(file, message_reply_to):
                 pg.mixer.music.play()
                 self.playing = True
-                if message_reply_to != None:
-                    await self.reply_to(message_reply_to, f"Now playing {self.current_track_number+1}/{len(self.playlist)}\n"+self.get_info_for_current_file())
-        elif message_reply_to != None:
-            await self.reply_to(message_reply_to, f"Skipped playing of "+self.playlist[self.current_track_number])
+                if message_reply_to is not None:
+                    await self.reply_to(
+                        message_reply_to,
+                        f"Now playing {self.current_track_number + 1}/{len(self.playlist)}\n"
+                        + self.get_info_for_current_file(),
+                    )
+        elif message_reply_to is not None:
+            await self.reply_to(
+                message_reply_to,
+                "Skipped playing of " + self.playlist[self.current_track_number],
+            )
         return self.playing
+
     async def pause(self, message_reply_to=None) -> None:
         pg.mixer.music.pause()
         self.playing = False
         self.start_playing = False
-        if message_reply_to != None:
+        if message_reply_to is not None:
             await self.reply_to(message_reply_to, f"Paused: {self.current_file}")
+
     async def unpause(self, message_reply_to=None) -> None:
         pg.mixer.music.unpause()
         self.playing = True
         self.start_playing = True
-        if message_reply_to != None:
+        if message_reply_to is not None:
             await self.reply_to(message_reply_to, f"Unpaused: {self.current_file}")
+
     def stop(self, message_reply_to=None) -> None:
         pg.mixer.music.stop()
         self.playing = False
         self.start_playing = False
-        if message_reply_to != None:
-            self.reply_to(message_reply_to, f"Stopped playing music")
+        if message_reply_to is not None:
+            self.reply_to(message_reply_to, "Stopped playing music")
+
     async def play_next(self, message_reply_to=None) -> None:
         self.current_track_number += 1
         if self.set_current_file(self.playlist[self.current_track_number]):
             await self.play(self.current_file, message_reply_to)
         else:
-            if message_reply_to != None:
-                await self.reply_to(message_reply_to, f"Skipped playing of "+self.playlist[self.current_track_number])
+            if message_reply_to is not None:
+                await self.reply_to(
+                    message_reply_to,
+                    "Skipped playing of " + self.playlist[self.current_track_number],
+                )
             await self.play_next(message_reply_to)
-    def set_volume(self, volume) -> None: # TODO: Fix setting of new volume. Why are numbers not round? Do them round.
+
+    def set_volume(
+        self, volume
+    ) -> (
+        None
+    ):  # TODO: Fix setting of new volume. Why are numbers not round? Do them round.
         pg.mixer.music.set_volume(volume / 100.0)
         self.current_volume = pg.mixer.music.get_volume() * 100
+
     def up(self) -> None:
         new_volume = int(min(100, self.current_volume + self.volume_increment))
         self.set_volume(new_volume)
+
     def down(self) -> None:
         new_volume = int(max(0, self.current_volume - self.volume_increment))
         self.set_volume(new_volume)
+
     def set_has_started(self, in_state: bool) -> None:
-        self.has_started = in_state  
+        self.has_started = in_state
+
     def set_current_file(self, file) -> bool:
         if self.file_is_ok(file):
             self.current_file = file
-            self.current_track_number = self.playlist.index(file)    
-            self.track_tags = TinyTag.get(os.path.join(self.music_directory, file)) 
+            self.current_track_number = self.playlist.index(file)
+            self.track_tags = TinyTag.get(os.path.join(self.music_directory, file))
             return True
         return False
+
     def get_info_for_current_file(self) -> str:
-        info: str = ''
-        if self.track_tags != None:
+        info: str = ""
+        if self.track_tags is not None:
             info = f"File name: {self.current_file}\nTitle: {self.track_tags.title}\nArtist: {self.track_tags.artist}\nAlbum: {self.track_tags.album}\nDuration: {time.strftime('%H:%M:%S', time.gmtime(self.track_tags.duration))}\n"
         return info
+
     def skip_track(self):
         pass
+
     def clear_statistics(self):
         self.statistics = {
-            "tracks": {
-                "played": 0,
-                "skipped": 0
-            },
-            "polls": {
-                "started": 0,
-                "passed": 0
-            }
+            "tracks": {"played": 0, "skipped": 0},
+            "polls": {"started": 0, "passed": 0},
         }
-        
